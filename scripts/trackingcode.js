b2460114b0b29ca47b70ebcafcc6389cca8fbe6e
Abhishek Dubey <Abhishek.Dubey@partner.bmwgroup.com>
2024-04-13 00:52:21 +0530

    [minor] added tracking code library and updated script in all files

---
+++ scripts/trackingcode.js
@@ -0,0 +1,12 @@
+(function() {
+    var gtag = document.createElement('script');
+    gtag.async = true;
+    gtag.src = 'https://www.googletagmanager.com/gtag/js?id=G-2EHBH9FZY5';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gtag, s);
+})();
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-2EHBH9FZY5');
