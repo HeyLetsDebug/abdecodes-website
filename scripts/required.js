6f171980cc2fbd776f18bb651746e5c73deda70a
Abhishek Dubey <Abhishek.Dubey@partner.bmwgroup.com>
2024-04-13 15:58:14 +0530

    [major] header & footer module creation, new blogs addition, bug fixes, css updates

--- scripts/required.js
+++ scripts/required.js
@@ -1,13 +1,57 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        const offset = window.innerWidth > 768 ? 90 : 0; // Adjust this value as needed
-        const target = document.querySelector(this.getAttribute('href'));
-        const targetOffset = target.offsetTop - offset;
+function includeHTML(filePath, container) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', filePath, true);
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        container.innerHTML = xhr.responseText;
+      } else {
+        console.error('Error fetching module:', filePath);
+      }
+    };
+    xhr.send();
+  }
+  
+  var headerContainer = document.getElementById('header-container');
+  var footerContainer = document.getElementById('footer-container');
+  var blogSidebar = document.querySelector('.blog-sidebar');
 
+  if(document.location.pathname === "/")
+  {
+  includeHTML('includes/header.html', headerContainer);
+  includeHTML('includes/footer.html', footerContainer);
+  }
+  else
+  {
+    includeHTML('../includes/header.html', headerContainer);
+    includeHTML('../includes/footer.html', footerContainer);
+    if(document.location.pathname.includes("/blogs/"))
+    {
+      includeHTML('../includes/sidebar.html', blogSidebar);
+    }
+  }
+
+  document.body.addEventListener('click', function(event) {
+    const target = event.target;
+
+    if (target.matches('a[href^="#"]')) {
+      event.preventDefault();
+      if(document.location.pathname === "/")
+      {
+        const offset = window.innerWidth > 768 ? 90 : 0;
+        var targetClicked = document.querySelector(target.getAttribute('href'));
+        const targetOffset = targetClicked.offsetTop - offset;
         window.scrollTo({
-            top: targetOffset,
-            behavior: 'smooth'
+          top: targetOffset,
+          behavior: 'smooth'
         });
-    });
-});
+      }
+      else
+      {
+        var sendTo = target.getAttribute('href');
+        window.location.href = "/"+sendTo;
+      }
+    } else if (target.id === "logoMain") {
+      event.stopPropagation();
+      window.location.href = "/";
+    }
+  });
